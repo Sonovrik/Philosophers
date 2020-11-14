ce6ce1f119bb3941eff52d2d487dc80d78d16528
Denis <lmidori@mi-h1.msk.21-school.ru>
2020-11-14 16:03:12 +0300

    need refact

--- philo_one/print_status.c
+++ philo_one/print_status.c
@@ -6,7 +6,7 @@
 /*   By: lmidori <lmidori@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:36:12 by lmidori           #+#    #+#             */
-/*   Updated: 2020/11/13 22:35:29 by lmidori          ###   ########.fr       */
+/*   Updated: 2020/11/14 15:56:25 by lmidori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,33 +44,57 @@ int		get_all_size(size_t val_time, size_t number, char *text)
 	int	len;
 
 	len = 0;
-	len += get_size_value(val_time + 1);
-	len += get_size_value(number + 1);
+	len += get_size_value(val_time) + 1;
+	len += get_size_value(number) + 1;
 	len += ft_strlen(text);
 	return (len);
 }
 
-void	init_string(int len, size_t val_time, size_t number, char *text)
+int		str_val_time(char *str, size_t val_time)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = get_size_value(val_time) + 1;
+	i = len - 1;
+	str[i] = ' ';
+	while (--i >= 0)
+	{
+		str[i] = val_time % 10 + 48;
+		val_time /= 10;
+	}
+	return (len);
+}
+
+
+void		str_text(char *str, char *text)
+{
+	int	i;
+
+	i = 0;
+	while (text[i] != '\0')
+	{
+		str[i] = text[i];
+		i++;
+	}
+	return ;
+}
+
+void	init_string(size_t val_time, size_t number, char *text)
 {
 	int		i;
 	int		len;
 	char	*str;
-	int		tmp;
 
 	i = 0;
 	len = get_all_size(val_time, number, text);
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	str = (char *)malloc(sizeof(char) * (len + 1)); // need guard
 	str[len] = '\0';
-	tmp = get_size_value(val_time);
-	while (tmp > 0)
-	{
-		str[tmp - 1] = val_time % 10 + 48;
-		val_time /= 10;
-		tmp--;
-	}
-	
-	
-	
+	i += str_val_time(&str[i], val_time);
+	i += str_val_time(&str[i], number);
+	str_text(&str[i], text);
+	ft_putstr_fd(str, 1);
 }
 
 void	view_status(t_philo *philo, int action)
@@ -78,64 +102,26 @@ void	view_status(t_philo *philo, int action)
 	int	len;
 	struct timeval	time;
 	size_t			val_time;
-	char			*str;
 
 	len = 0;
 	gettimeofday(&time, NULL);
 	val_time = (size_t)((time.tv_sec - philo->live_time->tv_sec) * 1000
 		+ (time.tv_usec - philo->live_time->tv_usec) / 1000);
-	len += get_size_value(val_time);
-	len += get_size_value((size_t)philo->number);
 	if (action == TOOK_FORK_LEFT)
-		
+		init_string(val_time, philo->number, "has taken left fork\n");
 	else if (action == TOOK_FORK_RIGHT)
-
+		init_string(val_time, philo->number, "has taken right fork\n");
 	else if (action == PUT_FORK_LEFT)
-
+		init_string(val_time, philo->number, "has drop left fork\n");
 	else if (action == PUT_FORK_RIGHT)
-
+		init_string(val_time, philo->number, "has drop right fork\n");
 	else if (action == SLEEPING)
-
+		init_string(val_time, philo->number, "is sleeping\n");
 	else if (action == EATING)
-
+		init_string(val_time, philo->number, "is eating\n");
 	else if (action == THINKING)
-
+		init_string(val_time, philo->number, "is thinking\n");
 	else if (action == DIED)
-
+		init_string(val_time, philo->number, "died\n");
 	
-	str = (char *)malloc(sizeof(char) * (len + 4));
-}
-
-void	view_status(t_philo *philo, int action)
-{
-	char	*str;
-	struct timeval time;
-	size_t	val_time;
-	
-	gettimeofday(&time, NULL);
-
-	val_time = (size_t)((time.tv_sec - philo->live_time->tv_sec) * 1000
-		+ (time.tv_usec - philo->live_time->tv_usec) / 1000);
-	// char	*tmp;
-	// char	*tmp2;
-	
-	str = ft_putnbr_fd(philo->number);
-	ft_putstr_fd("timestamp_in_ms ", 1);
-	// philo = NULL;
-	if (action == TOOK_FORK_LEFT)
-		ft_putstr_fd(" has taken left fork\n", 1);
-	else if (action == TOOK_FORK_RIGHT)
-		ft_putstr_fd(" has taken right fork\n", 1);
-	else if (action == PUT_FORK_LEFT)
-		ft_putstr_fd(" has drop left fork\n", 1);
-	else if (action == PUT_FORK_RIGHT)
-		ft_putstr_fd(" has drop right fork\n", 1);
-	else if (action == SLEEPING)
-		ft_putstr_fd(" is sleeping\n", 1);
-	else if (action == EATING)
-		ft_putstr_fd(" is eating\n", 1);
-	else if (action == THINKING)
-		ft_putstr_fd(" is thinking\n", 1);
-	else if (action == DIED)
-		ft_putstr_fd(" died\n", 1);
 }
